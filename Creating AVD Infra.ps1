7fad553708bad275c7c8af1dbcf799409c1ec2ed
ashwanirajput09 <116324500+ashwanirajput09@users.noreply.github.com>
2023-01-20 17:37:27 +0530

    Add files via upload

--- Creating AVD Infra.ps1
+++ Creating AVD Infra.ps1
@@ -9,10 +9,13 @@ $SubnetB="AVD_B"
 $SubnetB_prefix = "10.0.1.0/24"
 $SubA = New-AzVirtualNetworkSubnetConfig -Name $SubnetA -AddressPrefix $SubnetA_prefix
 $SubB = New-AzVirtualNetworkSubnetConfig -Name $SubnetB -AddressPrefix $SubnetB_prefix
+$Hostpool = "AVDHOSTPOOL"
+$workspace ='AVDWORKSPACE'
+$appgrpname ='AVDAPPGROUP'
 
 #Creating Resourse Group
 New-AzResourceGroup -Name $Resourcegroup -Location $Location
 #Creating vNet in Resource Group and adding subnet
 New-AzVirtualNetwork -Name $Network -ResourceGroupName $Resourcegroup -Location $Location -AddressPrefix $Address_Prefix -Subnet $SubA,$SubB
 
-New-AzWvdHostPool -ResourceGroupName $Resourcegroup -Name 'AVDHOSTPOOL' -WorkspaceName 'AVDWORKSPACE' -HostPoolType 'Pooled' -LoadBalancerType 'BreadthFirst' -Location $Location -DesktopAppGroupName 'AVDAPPGROUP' -PreferredAppGroupType 'Desktop'
+New-AzWvdHostPool -ResourceGroupName $Resourcegroup -Name $Hostpool -WorkspaceName $workspace -HostPoolType 'Pooled' -LoadBalancerType 'BreadthFirst' -Location $Location -DesktopAppGroupName $appgrpname -PreferredAppGroupType 'Desktop'
